0d26972afd8da147477d10285fa3deb0de081184
wen <wenyu1999@hotmail.com>
2018-02-18 20:51:57 -0500

    example code, yah?

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
-var x = 0;
-var y = 0;
+var context;
+var konva;
+var circle
+document.addEventListener('keydown', function(event) {
+    if(circle == null){
+        console.log('not here yet!');
+    }
+    else{
+        if(event.keyCode = 87){
+            circle.moveUp();
+        }
+    }
+});
 
-function keyPressed(){
-	var cir = document.getElementById("circle");
-	var shape = document.getElementById("svg");
-	var keycode = event.keyCode;
-	//alert(keycode);
-	if (keycode == 32){
-		cir.style.stroke = getRandomColor();
-	} else if (keycode == 115){
-		x = x + 50;
-		shape.style.marginTop = x + "px";
-	} else if (keycode == 119){
-		x = x - 50;
-		shape.style.marginTop = x + "px";
-	} else if (keycode == 100){
-		y = y + 50;
-		shape.style.marginLeft = y + "px";
-	} else if (keycode == 97){
-		y = y - 50;
-		shape.style.marginLeft = y + "px";
-	}
-	
-
-	
-
+window.onload = function() {
+    konva = document.getElementById('konva');
+    context = konva.getContext('2d');
+    circle = new Circle();
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+class Circle {
+    constructor() {
+        this.x = 200;
+        this.y = 200;
+        this.r = 50;
+        this.start = 0;
+        this.end = Math.PI*2;
+        this.color = "#ff6b6b";
+        this.draw();      
+    }
+
+    draw() {
+        context.strokeStyle = this.color;
+        context.beginPath();
+        context.arc(this.x, this.y, this.r, this.start, this.end);
+        context.stroke();
+    }
+    
+    erase() { 
+        context.clearRect(0, 0, konva.width, konva.height);
+    }
+
+    moveUp() {
+        console.log('sup');
+        this.erase();
+        this.y = this.y - 10;
+        this.draw();
+    }
 }
-
-
